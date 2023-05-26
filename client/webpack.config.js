32a15682210f670eb5ee0aaf47aa3430fe6daa31
bdalberson <Brian.alberson@gmail.com>
2023-05-25 19:26:11 -0700

    getting desktop icon working

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,9 +1,8 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const { GenerateSW, InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
@@ -26,6 +25,8 @@ module.exports = () => {
         swSrc: "./src-sw.js",
         swDest: "src-sw.js"
       }),
+
+      new GenerateSW(),
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true, 
